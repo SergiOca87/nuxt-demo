706216620cc0f2095e237317414234f764c376b5
Sergi <ocasergi@gmail.com>
2020-10-18 15:24:48 +0200

    offerings Array

--- nuxt-demo/pages/properties.vue
+++ nuxt-demo/pages/properties.vue
@@ -6,12 +6,16 @@
             <!-- basic search -->
             <div class="properties__form__search">
                 <div class="properties__form__search__item">
-                    <select name="'property-state'" v-model="property_city" v-on:change="filterProperties()">
+                    <select name="'property-state'" v-model="property_city" v-on:change="filterByCity()">
+                        <option value="all">Locations</option>
                         <option v-for="location in locations" :value="location">{{ location }}</option>
                     </select>
                 </div>
                 <div class="properties__form__search__item">
-                    <select name="'offering-type'"></select>
+                    <select name="'offering-type'" v-model="property_offering" v-on:change="filterByOffering()">
+                        <option value="all">Offering Type</option>
+                        <option v-for="offering in offerings" :value="offering">{{ offering }}</option>
+                    </select>
                 </div>
                 <div class="properties__form__search__item">
                     <select name="'property-type'"></select>
@@ -19,7 +23,6 @@
             </div>
         </form>
 
-  
         <template v-if="filteredProperties.length">
             <div class="post" v-for="property in filteredProperties" :key="property.id">
                 <h3>{{ property.title.rendered }}</h3>
@@ -46,6 +49,7 @@ export default {
     data() {
         return {
             property_city: '',
+            property_offering: ''
         }
     },
     created() {
@@ -58,6 +62,9 @@ export default {
         locations() {
             return this.$store.state.locations
         },
+        offerings() {
+            return this.$store.state.offerings
+        },
         filteredProperties() {
             return this.$store.state.filteredProperties
         }
@@ -66,8 +73,11 @@ export default {
         refresh() {
              this.$fetch()
         },
-        filterProperties() {
-            this.$store.dispatch('filterProperties', this.property_city)
+        filterByCity() {
+            this.$store.dispatch('filterByCity', this.property_city)
+        },
+         filterByOffering() {
+            this.$store.dispatch('filterByOffering', this.property_offering)
         }
     }
 }
--- nuxt-demo/store/index.js
+++ nuxt-demo/store/index.js
@@ -4,6 +4,7 @@ this is where we will eventually hold the data for all of our posts
 export const state = () => ({
     properties: [],
     locations: [],
+    offerings: [],
     filteredProperties: [],
 })
 
@@ -13,6 +14,9 @@ export const mutations = {
     },
     updateLocations: (state, locations) => {
         state.locations = locations
+    },
+    updateOfferings: (state, offering) => {
+        state.offerings.push(offering)
     },
     filterProperties: (state, results) => {
         state.filteredProperties = results
@@ -37,19 +41,51 @@ export const actions = {
                     acf,
                 }))
             
-            const locationsArr = [...new Set( properties.map( property => property.acf.location_tab_group.location_table.city ) )]
+            const locationsArr = [...new Set( properties.map( property => property.acf.location_tab_group.location_table.city ) )];
+            const offeringsArr =  [...new Set( properties.map( property => property.acf.general_tab_group.offering_type ) )];
+
+            // [53]
+            const convertedOfferings = offeringsArr.map((offering) => {
+                let offeringName = '';
+                (async () => {
+                    const offeringUrl = await fetch(
+                        `https://dev-nuxt-demo.pantheonsite.io/wp-json/wp/v2/offering-type/${offering}`
+                    ).then((res) => res.json())
+                    commit('updateOfferings', offeringUrl.name)
+                })();
+            })
+
             commit('updateProperties', properties)
             commit('updateLocations', locationsArr)
+            // commit('updateOfferings', convertedOfferings)
+         
             
         } catch (err) {
             console.log(err)
         }
     },
 
-    filterProperties({ state, commit }, location) {
+
+    filterByCity({ state, commit }, location) {
         const filteredResults = state.properties.filter(
             (property) =>
                 property.acf.location_tab_group.location_table.city === location
+        )
+        commit('filterProperties', filteredResults )
+    },
+    filterByOffering({ state, commit }, offering) {
+
+        const Sale = 53;
+        const Lease = 54;
+        let offeringNum;
+       
+        offering === 'Sale' ? offeringNum = 53 : offeringNum = 54
+
+        console.log('offering', offering, 'offeringNum', offeringNum)
+
+        const filteredResults = state.properties.filter(
+            (property) =>
+                property.acf.general_tab_group.offering_type === offeringNum
         )
         commit('filterProperties', filteredResults )
     },
